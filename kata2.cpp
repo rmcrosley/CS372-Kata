aefd9844ca691184e6ad5ec6b05d7ddf123c987d
Rachel Crosley <rmcrosley@alaska.edu>
2018-02-14 16:13:11 -0900

    all tests pass

--- kata2.cpp
+++ kata2.cpp
@@ -3,11 +3,15 @@
 // String Calculator Kata for CS372 from http://www.peterprovost.org/blog/2012/05/02/kata-the-only-way-to-learn-tdd/
 
 #include <string>
+#include<sstream>
 
 #include "catch.hpp"
 
 int stringCalculator(const std::string & input) {
-	return 0;
+    std::istringstream s(input);
+    int value;
+    s >> value;
+    return value;
 }
 
 TEST_CASE("An empty string returns zero.", "[empty]") {
